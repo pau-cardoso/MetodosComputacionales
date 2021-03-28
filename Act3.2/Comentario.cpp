b6989bb026d73d48819646e603b0fd4907e28e83
Pau <A01701490@itesm.mx>
2021-03-28 12:58:03 -0600

    Regresa posicion de aceptacion

--- Act3.2/Comentario.cpp
+++ Act3.2/Comentario.cpp
@@ -3,60 +3,65 @@
 
 using namespace std;
 
+const int STATE_A = 0;
+const int STATE_B = 1;
+const int STATE_C = 2;
+
 class Comentario {
 	private:
 		int state;
 	
 	public:
-		void state0(char);
-		void state1(char);
-		void state2(char);
-		void processEntry(string);
+		void stateA(char);
+		void stateB(char);
+		void stateC(char);
+		int processEntry(string);
 };
 
-void Comentario :: state0(char c) {
+void Comentario :: stateA(char c) {
 	if ( c == '/') {
-		state = 1;
+		state = STATE_B;
 	}
 	else state = -1;
 }
 
-void Comentario :: state1(char c) {
+void Comentario :: stateB(char c) {
 	if ( c == '/') {
-		state = 2;
+		state = STATE_C;
 	}
 	else state = -1;
 }
 
-void Comentario :: state2(char c) {
+void Comentario :: stateC(char c) {
 	if ( c != '\n') {
-		state = 2;
+		state = STATE_C;
 	}
-	else state = -1;
+	else state = -2;
 }
 
-void Comentario :: processEntry(string str) {
+int Comentario :: processEntry(string str) {
 	char c;
 	int i;
 
 	i = 0;
-	state = 0;
+	state = STATE_A;
 
 	while ( i < str.length() && state != -1 ) {
 		c = str[i];
 		if (c=='\n') break;
 		cout << "state = " << state << " c = ." << c << " \n";
 		switch (state) {
-			case 0: state0(c); break;
-			case 1: state1(c); break;
-			case 2: state2(c); break;
+			case STATE_A: stateA(c); break;
+			case STATE_B: stateB(c); break;
+			case STATE_C: stateC(c); break;
 		}
 		i++;
 	}
 
 	switch(state) {
-		case -1 : cout << "NOT ACCEPTED\n"; break;
-		case 2  : cout << "ACCEPTED\n"; break;
+		case STATE_C  : return i-1; break;
+		case -2 : return i-2; break;
+		default: return -1; break;
 	}
 }
 
@@ -65,5 +70,5 @@ int main(int argc, char* argv[]) {
 	string input;
 	cout << "Input: ";
 	getline(cin, input);
-	comment.processEntry(input);
+	cout << comment.processEntry(input) << endl;
 }
