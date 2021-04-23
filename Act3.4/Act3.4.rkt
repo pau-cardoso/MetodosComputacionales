d97d9fb90f54a57b38abd38e3662e53fdccfc99c
Pau <A01701490@itesm.mx>
2021-04-22 22:27:28 -0500

    avance

--- Act3.4/Act3.4.rkt
+++ Act3.4/Act3.4.rkt
@@ -1,5 +1,8 @@
 #lang racket
 
+;; Paulina Cardoso
+;; A01701490
+
 (require 2htdp/batch-io)
 
 ;; Lectura del archivo
@@ -30,38 +33,71 @@
     [else index]))
 
 
+
+;; Variables
+(define (variableA code index)
+  (cond
+    [(char-alphabetic? (car code)) (variableB (cdr code) (+ index 1))]
+    [else -1]))
+
+(define (variableB code index)
+  (cond
+    [(char-alphabetic? (car code)) (variableC (cdr code) (+ index 1))]
+    [(char-numeric? (car code)) (variableC (cdr code) (+ index 1))]
+    [(char-numeric? (car code)) (variableC (cdr code) (+ index 1))]
+    [else index]))
+
+(define (variableC code index)
+  (cond
+    [(char-alphabetic? (car code)) (variableC (cdr code) (+ index 1))]
+    [(char-numeric? (car code)) (variableC (cdr code) (+ index 1))]
+    [else index]))
+
+
+
 ;; Elimina los elementos de la lista dado un numero de posiciones a eliminar
 (define (popElement lst index)
   (cond
     [(> index 0) (popElement (cdr lst) (- index 1))]
     [else lst]))
 
-; (popElement prueba (commentA prueba 0))
+
+;; Dada una lista, convierte los elementos desde 0 hasta la posicion dada en un string
+(define (substring lst index textLst)
+  (cond
+    [(> index 0) (substring (cdr lst) (- index 1) (append textLst (list (car lst))))]
+    [else (list->string textLst)]))
 
 
-;; Corta la lista desde el indice dado hasta el final
-(define sublst
-  (lambda (lst newlst quantity)
-    (if (> quantity 0) (sublst (cdr lst) (append newlst (list (car lst))) (- quantity 1)) newlst)))
+;; Escribe el head del html
+(define headHTML
+  "<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Actividad 3.4 Paulina</title>
+    <link rel='stylesheet' href='styles.css'>
+</head>
+<body>")
 
-(define appendHTML
-  (lambda (openTag quantity lst html)
-    (string-append html (string-append openTag (string-append (list->string(sublst lst '() quantity)) "</span>")))))
 
-(define (writeTag class)
-  )
+;; Escribe el tag del html con su debida clase y texto
+(define (writeTag class text)
+  (string-append "<span class='" class "'>" text "</span>"))
 
-(define lexicalAnalyzer
-  (lambda (code html)
-    (if (empty? code) html
-        (cond
-          [(not (> (commentA code 0) 0)) (lexicalAnalyzer (lstPop code (commentA code 0)) (appendHTML "<span class='identifier'>" (commentA code 0) code html))]
-          ;[(not (equal? (floatA code 0) 0)) (lexicalAnalyzer (codePop code (floatA code 0)) (appendHTML "<span class='float'>" (floatA code 0) code html))]
-          ;[(not (equal? (intA code 0) 0)) (lexicalAnalyzer (codePop code (intA code 0)) (appendHTML "<span class='int'>" (intA code 0) code html))]
-          
-          [else (lexicalAnalyzer (cdr code) html)]
-         )
-    )))
+
+;; Realiza la escritura del resaltador de sintaxis en un string que se escribira en un archivo
+(define (syntaxisHighlighter code fileHTML)
+  (cond
+    [(empty? code) fileHTML]
+    [(equal? (car code) #\space) (syntaxisHighlighter (cdr code) (append fileHTML "&nbsp;"))]
+    [(equal? (car code) #\newline) (syntaxisHighlighter (cdr code) (append fileHTML "<br>"))]
+    [(equal? (car code) #\tab) (syntaxisHighlighter (cdr code) (append fileHTML "&nbsp;&nbsp;&nbsp;&nbsp;"))]
+    [(> (commentA code 0) 0) (syntaxisHighlighter (popElement code (commentA code 0)) (append (writeTag "comentario" (substring code (commentA code 0) '()))))]
+    [else (syntaxisHighlighter (cdr code) fileHTML)]))
+
 
 (define prueba
   '(#\/ #\/ #\space #\E #\s #\t #\e #\newline #\v #\a #\r #\1))
