69ae55d2450dfc8062a39ef3e1b106594105fbb1
Pau <A01701490@itesm.mx>
2021-04-25 15:49:42 -0500

    Palabras reservadas

--- Act3.4/Act3.4.rkt
+++ Act3.4/Act3.4.rkt
@@ -7,16 +7,146 @@
 
 ;; Lectura del archivo
 (define code
-  (string->list (read-file "D:\\Repositories\\MetodosComputacionales\\Act3.4\\test.cpp")))
+  (string->list (read-file "D:\\Repositories\\MetodosComputacionales\\Act3.4\\test.py")))
 
 ;; Escritura del archivo
 ; (write-file "prueba.html" fileHTML)
 ; copy ((syntaxisHighlighter code "") index.html)
 
+;; Palabras reservadas
+
+; int 
+(define (intPRA code index)
+  (cond
+    [(equal? (car code) #\i) (intPRB (cdr code) (+ index 1))]
+    [else -1]))
+
+(define (intPRB code index)
+  (cond
+    [(equal? (car code) #\n) (intPRC (cdr code) (+ index 1))]
+    [else -1]))
+
+(define (intPRC code index)
+  (cond
+    [(equal? (car code) #\t) (intPRD (cdr code) (+ index 1))]
+    [else -1]))
+
+(define (intPRD code index)
+  (cond
+    [(empty? code) index]
+    [(equal? (car code) #\space) index]
+    [else -1]))
+
+
+; in
+(define (inPRA code index)
+  (cond
+    [(equal? (car code) #\i) (intPRB (cdr code) (+ index 1))]
+    [else -1]))
+
+(define (inPRB code index)
+  (cond
+    [(equal? (car code) #\n) (+ index 1)]
+    [else -1]))
+
+
+; for
+(define (forPRA code index)
+  (cond
+    [(equal? (car code) #\f) (forPRB (cdr code) (+ index 1))]
+    [else -1]))
+
+(define (forPRB code index)
+  (cond
+    [(equal? (car code) #\o) (forPRC (cdr code) (+ index 1))]
+    [else -1]))
+
+(define (forPRC code index)
+  (cond
+    [(equal? (car code) #\r) (+ index 1)]
+    [else -1]))
+
+; return
+(define (returnPRA code index)
+  (cond
+    [(equal? (car code) #\r) (returnPRB (cdr code) (+ index 1))]
+    [else -1]))
+
+(define (returnPRB code index)
+  (cond
+    [(equal? (car code) #\e) (returnPRC (cdr code) (+ index 1))]
+    [else -1]))
+
+(define (returnPRC code index)
+  (cond
+    [(equal? (car code) #\t) (returnPRD (cdr code) (+ index 1))]
+    [else -1]))
+
+(define (returnPRD code index)
+  (cond
+    [(equal? (car code) #\u) (returnPRE (cdr code) (+ index 1))]
+    [else -1]))
+
+(define (returnPRE code index)
+  (cond
+    [(equal? (car code) #\r) (returnPRF (cdr code) (+ index 1))]
+    [else -1]))
+
+(define (returnPRF code index)
+  (cond
+    [(equal? (car code) #\n) (+ index 1)]
+    [else -1]))
+
+
+; char
+(define (charPRA code index)
+  (cond
+    [(equal? (car code) #\c) (charPRB (cdr code) (+ index 1))]
+    [else -1]))
+
+(define (charPRB code index)
+  (cond
+    [(equal? (car code) #\h) (charPRC (cdr code) (+ index 1))]
+    [else -1]))
+
+(define (charPRC code index)
+  (cond
+    [(equal? (car code) #\a) (charPRD (cdr code) (+ index 1))]
+    [else -1]))
+
+(define (charPRD code index)
+  (cond
+    [(empty? code) index]
+    [(equal? (car code) #\r) (+ index 1)]
+    [else -1]))
+
+
+;; Funcion que regresa la posicion donde llega la palabra reservada
+(define (palabraReservada code index)
+  (cond
+    [(empty? code) index]
+    ; int
+    [(> (intPRA code 0) 0) (intPRA code 0)]
+    ; in
+    [(> (inPRA code 0) 0) (inPRA code 0)]
+    ; for
+    [(> (forPRA code 0) 0) (forPRA code 0)]
+    ; char
+    [(> (charPRA code 0) 0) (charPRA code 0)]
+    ; return
+    [(> (returnPRA code 0) 0) (returnPRA code 0)]
+    ; Strings
+    ;[(> (intPRA code 0) 0) (intPRA code 0)]
+    ; Variables
+    ;[(> (intPRA code 0) 0) (intPRA code 0)]
+    [else -1]))
+
+
+
 ;; Comentario
 (define (commentA code index) 
   (cond 
-    [(equal? (car code) #\/) (commentB (cdr code) (+ index 1))]
+    [(equal? (car code) #\#) (commentC (cdr code) (+ index 1))]
     [else -1]))
 
 (define (commentB code index) 
@@ -202,6 +332,8 @@
     [(equal? (car code) #\tab) (syntaxisHighlighter (cdr code) (string-append fileHTML "&nbsp;&nbsp;&nbsp;&nbsp;"))]
     ; Comentarios
     [(> (commentA code 0) 0) (syntaxisHighlighter (popElement code (commentA code 0)) (string-append fileHTML (writeTag "comentario" (substring code (commentA code 0) '()))))]
+    ; Palabras reservadas
+    [(> (palabraReservada code 0) 0) (syntaxisHighlighter (popElement code (palabraReservada code 0)) (string-append fileHTML (writeTag "palabra-reservada" (substring code (palabraReservada code 0) '()))))]
     ; Strings
     [(> (stringA code 0) 0) (syntaxisHighlighter (popElement code (stringA code 0)) (string-append fileHTML (writeTag "string" (substring code (stringA code 0) '()))))]
     ; Variables
@@ -218,12 +350,11 @@
 
 
 (define prueba
-  '(#\" #\4 #\. #\3 #\e #\- #\5 #\3 #\space #\" #\newline #\h #\o #\l #\a #\;))
+  '(#\i #\n #\t #\space #\3 #\2 #\5 #\3 #\space #\newline #\h #\o #\l #\a #\;))
   ;;'(#\v #\a #\r #\1 #\_ #\space #\h))
   ;'(#\/ #\/ #\space #\E #\s #\t #\e #\newline #\v #\a #\r #\1))
 
 ;; '(#\/ #\/ #\space #\E #\s #\t #\e)
 ;; (commentA prueba 0)
 
-; (write-file "prueba.html" fileHTML)
 ;;(syntaxisHighlighter code headHTML)
